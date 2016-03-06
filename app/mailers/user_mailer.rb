737eef498acd9bab9343326e7113de21b7443a49
CharlesMassey <chasemassey@gmail.com>
2016-03-06 18:43:46 -0500

    Adding maul gun to app.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+  default from: "from@example.com"
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def receive(email)
+    @user = email.sender
+    @topic = email.subject
+    @url = params["body-plain"]
+  end
+end
