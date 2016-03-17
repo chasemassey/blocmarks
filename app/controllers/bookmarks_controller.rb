0475dada7326dcdc47637940e60d5071f1497ea3
CharlesMassey <chasemassey@gmail.com>
2016-03-17 15:14:14 -0400

    finished pundit

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,61 @@
 class BookmarksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def show
+    @topic = Topic.find(params[:topic_id]) #this line might be unnecessary, could do @bookmark.topic
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.new
+    authorize @bookmark
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = @topic.bookmarks.build(bookmark_params)
+    @bookmark.user = current_user
+    authorize @bookmark
+
+    if @bookmark.save
+      flash[:notice] = "Bookmark saved"
+      redirect_to [@topic, @bookmark]
+    else
+      flash[:alert] = "Something went wrong"
+      render :new
+    end
   end
 
   def edit
+    @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
   end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
+
+    if @bookmark.update(bookmark_params)
+      flash[:notice] = "Updated Bookmark"
+      redirect_to [@bookmark.topic, @bookmark]
+    else
+      flash[:alert] = "Something went wrong"
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
+    @bookmark.destroy
+    flash[:notice] = "Bookmark Deleted"
+    redirect_to @bookmark.topic
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:url)
+  end
+
 end
--- app/controllers/incoming_controller_controller.rb
+++ app/controllers/incoming_controller_controller.rb
@@ -1,22 +1,40 @@
 class IncomingControllerController < ApplicationController
 
-# http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
+  # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
 
   def create
+    title = params[:message][:headers][:subject]
+    email = params[:envelope][:sender]
+    @user = User.find_by(email: email)
+    @topic = Topic.find_by(title: title)
+    @url = params['body-plain']
 
 
-    # You put the message-splitting and business
-    # magic here.
-      # Find the user by using params[:sender]
-      # Find the topic by using params[:subject]
-      # Assign the url to a variable after retreiving it from params["body-plain"]
+    if User.nil?
+      @user = User.create!(email: email, password: 'password')
+      @user.skip_confirmation!
+      @user.save!
+    else
+      user = @user
+    end
 
-      # Check if user is nil, if so, create and save a new user
+    if Topic.nil?
+      @topic = Topic.create!(title: title, user: @user)
+    end
 
-      # Check if the topic is nil, if so, create and save a new topic
+    Bookmark.create!(topic: @topic, user: @user, url: @url)
+    # Find the user by using params[:sender]
+    # Find the topic by using params[:subject]
+    # Assign the url to a variable after retreiving it from params["body-plain"]
 
-      # Now that you're sure you have a valid user and topic, build and save a new bookmark
+    # Check if user is nil, if so, create and save a new user
+
+    # Check if the topic is nil, if so, create and save a new topic
+
+    # Now that you're sure you have a valid user and topic, build and save a new bookmark
+
     # Assuming all went well.
     head 200
   end
