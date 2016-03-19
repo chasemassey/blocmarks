36ab40839dc8881dc40182589b4cb36756654790
CharlesMassey <chasemassey@gmail.com>
2016-03-19 17:12:02 -0400

    finished checkpoint 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-         has_many :topics, dependent: :destroy
+  has_many :topics, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_bookmarks, through: :likes, source: :bookmark
+
+  def liked(bookmark)
+    likes.where(bookmark_id: bookmark.id).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'likes/index'
+
+  get 'users/show'
+
     devise_for :users
     resources :users, only: [:show] do
+      get 'welcome/index'
     resources :topics
+      resources :bookmarks, except: [:index] do
+        resources :likes, only: [:index, :create, :destroy]
+    end
   end
 
   post :incoming, to: 'incoming#create'
