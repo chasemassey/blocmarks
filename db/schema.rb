88a29cb1eeddc5cd0af8b6358d8bdf41b90ea33c
chasemassey <chasemassey@gmail.com>
2016-04-26 18:20:43 +0000

    db stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325201734) do
+ActiveRecord::Schema.define(version: 20160324005105) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
@@ -30,9 +30,6 @@ ActiveRecord::Schema.define(version: 20160325201734) do
     t.integer  "bookmark_id"
     t.integer  "user_id"
   end
-
-  add_index "likes", ["bookmark_id"], name: "index_likes_on_bookmark_id"
-  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
@@ -63,7 +60,6 @@ ActiveRecord::Schema.define(version: 20160325201734) do
     t.string   "name"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
